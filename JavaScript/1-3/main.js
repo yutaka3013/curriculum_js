fefc73e7b78dc535e5aaf0a0ba2140d060f34e4a
yutaka <68794470+yutaka3013@users.noreply.github.com>
2020-08-16 20:51:17 +0900

    条件分岐・演算子
    ご確認ください。
    よろしくお願いいたします。

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,34 @@
+//問1：次の条件を、論理演算子を使って表してください。
+
+let a = 9;
+if (a != 10) {
+    console.log(a);  
+}
+
+
+let b = 14;
+if (b >= 10 && b <= 20) {
+    console.log(b);  
+}
+
+
+//問2：以下の条件を満たすように、記述してください
+
+
+let score = 10;
+if (score >= 10 && score <= 20) {
+    console.log('成功です');
+} else {
+    console.log('失敗です');  
+}
+
+
+//問3：以下の条件を満たすように、記述してください
+
+let number = 0;
+
+if (number % 2 === 0) {
+    console.log('偶数です');
+} else {
+	console.log('奇数です');
+}
